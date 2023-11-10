9979f018dc9ad3cf75860a8ed7cef0a0834526bc
vyacheslavsafin <safin.vr@mail.ru>
2023-11-10 21:34:33 +0300

    Добавлены модели Курс, Урок

--- education/models.py
+++ education/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание курса', **NULLABLE)
+    preview = models.ImageField(upload_to='education/', verbose_name='Превью курса', **NULLABLE)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание урока', **NULLABLE)
+    preview = models.ImageField(upload_to='education/', verbose_name='Превью урока', **NULLABLE)
+    video_link = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
