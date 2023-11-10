17397706b63a7f49efcb7c30084c49f1ecac5980
vyacheslavsafin <safin.vr@mail.ru>
2023-11-10 21:26:34 +0300

    Добавлены поля к модели пользователя. Создано приложение Education.

---
+++ education/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from education.models import NULLABLE
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=25, verbose_name='телефон', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
